87bf7e8a7247e59d9470ffb933130724525d1de4
Matthew Goslett <matthew@goslett.co.za>
2017-05-04 13:40:07 +0200

    export PubSubAdapterInterface from module

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 
 /** @module js-pubsub */
 
+let PubSubAdapterInterface = require('./src/PubSubAdapterInterface');
 let DevNullPubSubAdapter = require('./src/DevNullPubSubAdapter');
 let LocalPubSubAdapter = require('./src/LocalPubSubAdapter');
 let Utils = require('./src/Utils');
 
+module.exports.PubSubAdapterInterface = PubSubAdapterInterface;
 module.exports.DevNullPubSubAdapter = DevNullPubSubAdapter;
 module.exports.LocalPubSubAdapter = LocalPubSubAdapter;
 module.exports.Utils = Utils;
